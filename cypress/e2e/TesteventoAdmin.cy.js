44430754341cc119f1755debad742ef1b577bc51
RulyCisnero <rulycisnero@gmail.com>
2025-09-13 14:50:28 -0300

    Diseño TC's

--- cypress/e2e/TesteventoAdmin.cy.js
+++ cypress/e2e/TesteventoAdmin.cy.js
@@ -20,18 +20,22 @@ describe('Tareas relacionadas con el admin', () => {
   })
 
 
-  it('Deberia buscar a la persona que organiza el evento para dar de alta ', () => {
+  it('TC-001: En Creado, buscar nombre del evento X → cambiar a Rechazado → verificar en Rechazado si se encuentra el evento X', () => {
+    //Nombre del Evento: F90
+    //Cambiar estado Creado → Rechazado
     cy.get('[type="button').eq(1).click();
     cy.wait(1000)
     evento.clickAdminEventos();
     cy.wait(5000)
     evento.bttnCreado().click();
     cy.wait(2000);
-    evento.cambiarEstadoDeEventoPorTexto('🎉💀ñ@#', 'Aprobado');
+    //evento.cambiarEstadoDeEventoPorTexto('🎉💀ñ@#', 'Rechazado');
+    evento.cambiarEstadoDeEventoPorTexto('F90', 'Rechazado');
     cy.wait(2000)
     evento.clickModalAceptar();
     cy.wait(2000)
-
+    evento.bttnRechazado().click();
+    evento.verificarTextoEnGrilla('F90');
     /* 
     evento.obtenerFilaPorTexto('MegaTest').within(() => {
       cy.get('[data-cy^="select-estado-"]').click();
@@ -43,13 +47,59 @@ describe('Tareas relacionadas con el admin', () => {
 
   });
 
- /*  it('Cambiando el estado del cliente', () => {
+  it('TC-002: En Rechazado, buscar Email del creador del evento X → cambiar a Aprobado → verificar en Aprobado si se encuentra el evento X', () => {
+    //Email creador del Evento: raulecisnero@gmail.com
+    //Cambiar estado Rechazado → Aprobado
+    cy.get('[type="button').eq(1).click();
+    cy.wait(1000)
+    evento.clickAdminEventos();
+    cy.wait(5000)
+    evento.bttnRechazado().click();
+    cy.wait(2000);
+    evento.cambiarEstadoDeEventoPorTexto('raulecisnero@gmail.com', 'Aprobado');
+    cy.wait(2000)
+    evento.clickModalAceptar();
+    cy.wait(2000)
+    evento.bttnAprobado().click();
+    evento.verificarTextoEnGrilla('raulecisnero@gmail.com');
+  });
+
+  it('TC-003:En Completado, buscar evento por su nombre X → Cambiar estado a Pospuesto → cancelar modal', () => {
+    //Nombre del Evento: Grupo 7
+    //Cambiar estado Completado → Pospuesto 
+    cy.get('[type="button').eq(1).click();
+    cy.wait(1000)
+    evento.clickAdminEventos();
+    cy.wait(5000)
+    evento.bttnCompletado().click();
+    cy.wait(2000);
+    evento.cambiarEstadoDeEventoPorTexto('Grupo 7', 'Pospuesto');
+    cy.wait(2000)
+    evento.clickModalCancelar();
+    cy.wait(2000)
+    evento.verificarTextoEnGrilla('Grupo 7');
+  });
+
+  it('TC-001: En Pendiente, buscar cliente por Nombre X → cambiar a Rechazado → verificar en Rechazado si se encuentra el cliente X.', () => {
     cy.get('[type="button').eq(1).click();
     cy.wait(1000)
     cliente.clickAdminClientes();
     cy.wait(6000);
     cliente.bttnPendiente().click();
     cliente.cambiarEstadoDeClientePorTexto('@¦¦', 'Rechazado');
+    cy.wait(1000)
+    cliente.clickModalAceptar();
+    cliente.bttnRechazado().click();
+    cliente.verificarTextoEnGrilla('@¦¦');
+  });
+
+  it('TC-002: En Rechazado, buscar cliente por Email X → cambiar a Aprobado → verificar en Aprobado si se encuentra el cliente X.', () => {
+    cy.get('[type="button').eq(1).click();
+    cy.wait(1000)
+    cliente.clickAdminClientes();
+    cy.wait(6000);
+    cliente.bttnRechazado().click();
+    cliente.cambiarEstadoDeClientePorTexto('@¦¦', 'Pendiente');
     cy.wait(1000)
     cliente.clickModalAceptar();
   });
@@ -63,5 +113,6 @@ describe('Tareas relacionadas con el admin', () => {
     cliente.cambiarEstadoDeClientePorTexto('@¦¦', 'Pendiente');
     cy.wait(1000)
     cliente.clickModalAceptar();
-  }); */
+  });
+
 })
